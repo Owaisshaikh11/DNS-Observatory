662b7b134d1a4974f774d0ce04aeda46c7827d45
Owais shaikh <Owaisshaikh117@outlook.com>
2026-07-01 02:20:32 +0530

    chore(devops): add Vercel Web Analytics tracking

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 )
